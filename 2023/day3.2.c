d1e31672c5a022b0a15e6416b2e6104bcb73cb88
M47Z <38143854+IamM47Z@users.noreply.github.com>
2024-01-05 20:13:28 +0000

    day3

---
+++ 2023/day3.2.c
@@ -0,0 +1,155 @@
+/*
+*   Advent of Code Challenge - Day 3 - Part Two
+*/
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#include "../utils.h"
+
+int getNumLines(const char* buffer, const int size)
+{
+    int num = 0;
+
+    for (int i = 0; i < size; i++)
+        num += (buffer[i] == '\n');
+    
+    return ++num;
+}
+
+int getNumCols(const char* buffer, const int size)
+{
+    for (int i = 0; i < size; i++)
+        if (buffer[i] == '\n')
+            return i;
+    
+    return -1;
+}
+
+const char* getNumStart(const char* buffer, const char* start)
+{
+    while (buffer > start && isdigit(buffer[-1]))
+        --buffer;
+    
+    return buffer;
+}
+
+int getNumDigits(const char* buffer, const int size)
+{
+    int num = 0;
+
+    while (num < size && isdigit(buffer[num]))
+        ++num;
+    
+    return num;
+}
+
+int getNumber(const char* buffer, const int size)
+{
+    int num = 0;
+
+    for (int i = 0; i < size; ++i)
+    {
+        const int curr = buffer[i] - 0x30;
+        num += curr * pow(10, (size - i - 1));
+    }
+
+    return num;
+}
+
+int getGearRatio(const char* buffer, const int l, const int c, const int num_lines, const int num_cols)
+{
+    int num = 0;
+    int mult = 1;
+
+    for (int j = -1; j < 2; ++j)
+    {
+        const int line = l + j;
+        if (line < 0)
+            continue;
+
+        if (line > num_lines)
+            break;
+
+        bool found = false;
+        for (int k = -1; k < 2; ++k)
+        {
+            const int col = c + k;
+            if (col < 0)
+                continue;
+
+            if (col > num_cols)
+                break;
+
+            const char* elem = &buffer[(num_cols + 1) * line + col];
+            if (!isdigit(*elem))
+            {
+                found = false;
+                continue;
+            }
+
+            if (found)
+                continue;
+
+            const char* start = getNumStart(elem, buffer);
+            const int num_digits = getNumDigits(start, num_cols - col - (elem - start));
+            const int number = getNumber(start, num_digits);
+
+            mult *= number;
+                
+            found = true;
+            ++num;
+        }
+    }
+
+    return num == 2 ? mult : -1;
+}
+
+
+int main(void)
+{
+    char* data = NULL;
+    int data_size = getDayInput(3, &data);
+    if (data_size == -1)
+        return 1;
+
+    const int num_lines = getNumLines(data, data_size);
+    const int num_cols = getNumCols(data, data_size);
+
+    printf("Size: %dx%d\n", num_lines, num_cols);
+
+    int sum = 0;
+    for (int l = 0, c = 0; l < num_lines;)
+    {
+        if (c > num_cols)
+        {
+            c = 0;
+            ++l;
+            continue;
+        }
+
+        const char* curr = &data[(num_cols + 1) * l + c];
+        const int curr_size = num_cols - c;
+
+        if (*curr != '*')
+        {
+            ++c;
+            continue;
+        }
+
+        const int ratio = getGearRatio(data, l, c, num_lines, num_cols);
+        if (ratio != -1)
+        {
+            printf("Adding %d from %dx%d.\n", ratio, l + 1, c + 1);
+            sum += ratio;
+        }
+
+        ++c;
+    }
+
+    printf("\nSum of all of the gear rations in the engine schematic: %d\n", sum);
+
+    free(data);
+
+    return 0;
+}
---
+++ 2023/day3.c
@@ -0,0 +1,133 @@
+/*
+*   Advent of Code Challenge - Day 3
+*/
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#include "../utils.h"
+
+int getNumLines(const char* buffer, const int size)
+{
+    int num = 0;
+
+    for (int i = 0; i < size; i++)
+        num += (buffer[i] == '\n');
+    
+    return ++num;
+}
+
+int getNumCols(const char* buffer, const int size)
+{
+    for (int i = 0; i < size; i++)
+        if (buffer[i] == '\n')
+            return i;
+    
+    return -1;
+}
+
+int getNumDigits(const char* buffer, const int size)
+{
+    int num = 0;
+
+    while (num < size && isdigit(buffer[num]))
+        ++num;
+    
+    return num;
+}
+
+bool hasNeighbourSymbol(const char* buffer, const int l, const int c, const int num_digits, const int num_lines, const int num_cols)
+{
+    const char default_value = '.';
+
+    for (int i = 0; i < num_digits; ++i)
+    {
+        for (int j = -1; j < 2; ++j)
+        {
+            const int line = l + j;
+            if (line < 0)
+                continue;
+
+            if (line > num_lines)
+                break;
+
+            for (int k = -1; k < 2; ++k)
+            {
+                const int col = c + k + i;
+                if (col < 0)
+                    continue;
+
+                if (col > num_cols)
+                    break;
+
+                const char value = buffer[(num_cols + 1) * line + col];
+                if (!ispunct(value))
+                    continue;
+
+                if (value != default_value)
+                    return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int getNumber(const char* buffer, const int size)
+{
+    int num = 0;
+
+    for (int i = 0; i < size; ++i)
+    {
+        const int curr = buffer[i] - 0x30;
+        num += curr * pow(10, (size - i - 1));
+    }
+
+    return num;
+}
+
+int main(void)
+{
+    char* data = NULL;
+    int data_size = getDayInput(3, &data);
+    if (data_size == -1)
+        return 1;
+
+    const int num_lines = getNumLines(data, data_size);
+    const int num_cols = getNumCols(data, data_size);
+
+    printf("Size: %dx%d\n", num_lines, num_cols);
+
+    int sum = 0;
+    for (int l = 0, c = 0; l < num_lines;)
+    {
+        if (c > num_cols)
+        {
+            c = 0;
+            ++l;
+            continue;
+        }
+
+        const char* curr = &data[(num_cols + 1) * l + c];
+        const int curr_size = num_cols - c;
+
+        const int num_digits = getNumDigits(curr, curr_size);
+        if (num_digits == 0 || !hasNeighbourSymbol(data, l, c, num_digits, num_lines, num_cols))
+        {
+            ++c;
+            continue;
+        }
+
+        sum += getNumber(curr, num_digits);
+
+        printf("Adding %d from %dx%d with %d digits.\n", getNumber(curr, num_digits), l + 1, c + 1, num_digits);
+
+        c += num_digits;
+    }
+
+    printf("\nSum of all of the part numbers in the engine schematic: %d\n", sum);
+
+    free(data);
+
+    return 0;
+}
